97d53833da84f1adbc06b5f9d1c5c12cef46c85a
Mariusz Kogen <k0gen@pm.me>
2025-04-11 16:10:12 +0200

    10.10.7 Update

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM jellyfin/jellyfin:10.10.6
+FROM jellyfin/jellyfin:10.10.7
 
 ARG DEBIAN_FRONTEND="noninteractive"
 
--- scripts/procedures/migrations.ts
+++ scripts/procedures/migrations.ts
@@ -36,5 +36,5 @@ export const migration: T.ExpectedExports.migration = compat.migrations
             },
           },
         },
-    "10.10.6",
+    "10.10.7",
 );
